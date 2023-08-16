bc9e9383510f4d72e6c5211dabb76ccc0b27391b
Adithya Balagoni <adithyabalagoni11@gmail.com>
2023-08-16 12:34:43 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 10:43:16 2023
+
+@author: adith
+"""
+
+import streamlit as st 
+
+
+st.title('IPL Stats Page')
+
+st.markdown("[Batter Stats(Individual)](https://iplbatters.streamlit.app/)")
+st.write('Analyse batters stats by phases,bowling types')
+
+st.markdown("[Bowler Stats(Individual)](https://iplbowlers.streamlit.app/)")
+st.write('Analyse batters stats by phases,bowling types')
+
+
+st.markdown("[Batters Comparison](https://iplbowlers.streamlit.app/)")
+st.write('Compare batters by phases,bowling types')
+
+st.markdown("[Bowlers Comparison](https://iplbowlerscomparision.streamlit.app/)")
+
+st.write('Compare bowlers by phases,bowling types')
