f3a2bb28692c7c0c5dffc5ae0ab254d08dddb130
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2023-08-16 13:06:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ st.markdown("[Bowler Stats(Individual)](https://iplbowlers.streamlit.app/)")
 st.write('Analyse batters stats by phases,bowling types')
 
 
-st.markdown("[Batters Comparison](https://iplbowlers.streamlit.app/)")
+st.markdown("[Batters Comparison](https://iplbatterscomparison.streamlit.app/)")
 st.write('Compare batters by phases,bowling types')
 
 st.markdown("[Bowlers Comparison](https://iplbowlerscomparision.streamlit.app/)")
